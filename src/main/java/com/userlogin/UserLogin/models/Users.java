53a82e8b3da3400ee0d891ad22d439b0a34a4aa9
Vijay Kumar <vijay.kr@unibrain.in>
2025-04-10 19:23:48 +0530

    testing

--- src/main/java/com/userlogin/UserLogin/models/Users.java
+++ src/main/java/com/userlogin/UserLogin/models/Users.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Users {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
